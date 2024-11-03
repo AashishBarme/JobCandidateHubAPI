5e0209ccee4c2ec52e3903eca0d59d364a9f7794
AashishBarme <aashish.barme9@gmail.com>
2024-11-03 17:51:35 +0545

    added entity

---
+++ JobCandidateHubAPI/Infrastructure/Entities/JobCandidate.cs
@@ -0,0 +1,15 @@
+﻿namespace JobCandidateHubAPI.Infrastructure.Entities
+{
+    public class JobCandidate
+    {
+        public long Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public string? TimeInterval { get; set; }
+        public string? LinkedinProfileUrl { get; set; }
+        public string? GithubProfileUrl { get; set; }
+        public string? Comment { get; set; }
+    }
+}
