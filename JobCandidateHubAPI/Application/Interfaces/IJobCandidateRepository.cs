dc7b0f1eb529456c3e45c47597e53ada8ce7c305
AashishBarme <aashish.barme9@gmail.com>
2024-11-03 18:56:42 +0545

    added interface for repository

---
+++ JobCandidateHubAPI/Application/Interfaces/IJobCandidateRepository.cs
@@ -0,0 +1,12 @@
+﻿using JobCandidateHubAPI.Infrastructure.Entities;
+using JobCandidateHubAPI.Models;
+
+namespace JobCandidateHubAPI.Application.Interfaces
+{
+    public interface IJobCandidateRepository
+    {
+        public Task<long> Create(JobCandidateDto dto);
+        public Task<long> Update(JobCandidateDto dto);
+        public Task<long> GetCandidateIdByEmail(string email);
+    }
+}
--- JobCandidateHubAPI/Models/JobCandidateDto.cs
+++ JobCandidateHubAPI/Models/JobCandidateDto.cs
@@ -1,15 +1,21 @@
-﻿namespace JobCandidateHubAPI.Infrastructure.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobCandidateHubAPI.Models
 {
-    public class JobCandidate
+    public class JobCandidateDto
     {
         public long Id { get; set; } = 0;
+        [Required]
         public string? FirstName { get; set; }
+        [Required]
         public string? LastName { get; set; }
         public string? PhoneNumber { get; set; }
+        [Required]
         public string? Email { get; set; }
         public string? TimeInterval { get; set; }
         public string? LinkedinProfileUrl { get; set; }
         public string? GithubProfileUrl { get; set; }
+        [Required]
         public string? Comment { get; set; }
     }
 }
